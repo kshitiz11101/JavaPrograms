d958a1c3539a68983769f54fef959d01c710508a
Kshitiz Sharma <kshitiz01sharma@gmail.com>
2024-08-22 13:48:39 +0530

    DataTypes commit

---
+++ src/DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes{
+    public static void main(String[] args) {
+//        byte b=128;  // incompatible types range of byte--> -127 to 127
+//        short b=128;   // short range --> -2^15 to 2^15-1
+//        short b=true// incompatible types
+//        int b=2020202022;  // int range--> -2^31 to 2^31-1
+//       long b=2020202022222222222L;  // long range--> 2^63 to 2^63-1
+//        float b= 3.3F;  // float range --> -3.4e38 to 3.4e38
+//        double b=3.4444464654322222222222;   //double range--> -1.7e308 to 1.7e308
+//        boolean b=True;   //   error: cannot find symbol
+//        boolean b=0;   // incompatible types
+//        boolean b="true";     // incompatible types
+//        boolean b=true;
+//        char b=a;  // cannot find symbol
+        char b='a';
+//        char b="a"; // incompatible types
+//      char b='ab';   // unclosed character literal
+//        char b=1;  //    as output
+//        char b=22222;   // 囎 as output
+        System.out.println(b);
+    }
+}
+
+
