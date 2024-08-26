01dfcd56e3eb72636f99afc7ed2d869dc5c1d548
Kshitiz Sharma <kshitiz01sharma@gmail.com>
2024-08-26 15:51:45 +0530

    WrapperClasses Commit

---
+++ src/WrapperClasses.java
@@ -0,0 +1,11 @@
+public class WrapperClasses {
+    public static void main(String[] args) {
+//        Integer i=new Integer(50);
+//        System.out.println(i.getClass());  // class java.lang.Integer
+//        Float f=new Float(3.4F);
+//        System.out.println(i.byteValue());
+//        System.out.println(i.doubleValue());
+        int x=new Integer.parseInt("10"); // java: cannot find symbol symbol:   class parseInt
+        System.out.println(x);
+    }
+}
