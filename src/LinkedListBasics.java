ef1015e742330032e8cc5a2a1d47334b98032b4d
Kshitiz Sharma <kshitiz01sharma@gmail.com>
2024-08-28 10:10:22 +0530

    LinkedListBasics Commit

---
+++ src/LinkedListBasics.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class LinkedListBasics {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addFirst(10);
+        list.addFirst(20);
+        list.addFirst(null);
+        System.out.println(list);
+
+    }
+}
